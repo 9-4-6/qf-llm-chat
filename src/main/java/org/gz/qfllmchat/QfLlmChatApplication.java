aaf80f798eec7dccfa841941d826b2c660ad0e52
guozhong <17853522814@163.com>
2025-12-10 13:13:02 +0800

    init

---
+++ src/main/java/org/gz/qfllmchat/QfLlmChatApplication.java
@@ -0,0 +1,13 @@
+package org.gz.qfllmchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfLlmChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QfLlmChatApplication.class, args);
+    }
+
+}

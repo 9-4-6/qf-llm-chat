f85700da248775b045df9ebb3cfd67fbdd653598
guozhong <17853522814@163.com>
2025-12-10 13:51:51 +0800

    commit:init

---
+++ src/main/java/org/gz/qfllmchat/config/LangChainConfig.java
@@ -0,0 +1,9 @@
+package org.gz.qfllmchat.config;
+
+/**
+ * @author guozhong
+ * @date 2025/12/10
+ * @description langChain配置
+ */
+public class LangChainConfig {
+}
---
+++ src/main/java/org/gz/qfllmchat/controller/ChatController.java
@@ -0,0 +1,9 @@
+package org.gz.qfllmchat.controller;
+
+/**
+ * @author guozhong
+ * @date 2025/12/10
+ * @description TODO
+ */
+public class ChatController {
+}
